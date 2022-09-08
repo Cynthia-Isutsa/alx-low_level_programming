97eb0ce9cbff02728e159445c459ae7490141eff
Cynthia-Isutsa <isutsacynthia@gmail.com>
2022-09-08 15:57:19 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Description: prints using prinf
+ * Not allowed to use puts
+ * returns 0
+ */
+
+int main(void)
+	{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+
+	}
