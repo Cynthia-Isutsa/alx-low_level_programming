3bfc7bf2c2976bc1b9a376c9c124dd69f9ef08e5
Cynthia-Isutsa <isutsacynthia@gmail.com>
2022-09-08 15:24:37 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**main-The entry point for the program
- * Return:By default returns zero if no errors otherwise returns non-zero value
+ * Description : C program that prints with puts function
+ * printf not allowed
+ * By default returns 0
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
