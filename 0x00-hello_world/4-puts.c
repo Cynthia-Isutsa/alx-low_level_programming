2e5722ac7d48da7a6d673c89a6439f51eb27c965
Cynthia-Isutsa <isutsacynthia@gmail.com>
2022-09-08 20:23:58 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Description : C program that prints with puts function
- * printf not allowed
- * By default returns zero
+ * main - Entry point
+ * Description: A C program that prints with put function
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-	
-	puts("\"Programming is like building a multilingual puzzle");
-	
-		return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 
 }
