17e58d04e82c0d0306a3417bd6380d733f722c8e
Cynthia-Isutsa <isutsacynthia@gmail.com>
2022-09-15 03:56:51 -0700

    unstashed

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - prints alphabets in lower case and upper case
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+	/*Prints a-z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+	/*prints A-Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		++CH;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*convert digit to ASCII*/
+		putchar(digit + '0');
+		++digit;
+
+	}
+	putchar('\n');
+
+	return (0);
+}
