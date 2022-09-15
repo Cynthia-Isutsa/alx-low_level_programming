0fc1f83302c4be27203bff2cfc7ab9bfed2c2728
Cynthia-Isutsa <isutsacynthia@gmail.com>
2022-09-15 03:08:54 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(string[ch]);
+	_putchar('\n');
+
+	return (0);
+}
