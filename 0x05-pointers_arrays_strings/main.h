70e44bff9d4734a105921b2bd9c7366244a7b4c0
Cynthia-Isutsa <isutsacynthia@gmail.com>
2022-09-19 19:56:31 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,41 @@
+#ifndef MAIN_H
+#det_to_98 - takes a pointer to an int parameter
+
+ *               and updates the value it points to 98
+ *
+ *                *
+ *
+ *                 * @n: int parameter
+ *
+ *                  *
+ *
+ *                   * Return: Nothing
+ *
+ *                    */
+ 
+ 
+ 
+ void reset_to_98(int *n)
+	
+{
+	
+		*n = 98;
+		
+}fine MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif /* MAIN_H */
