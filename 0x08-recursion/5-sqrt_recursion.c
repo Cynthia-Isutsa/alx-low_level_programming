f886a9fc174a72bbfcd97cf6ce83ce793257cddf
Cynthia-Isutsa <isutsacynthia@gmail.com>
2022-09-30 09:44:23 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _sqrt_recursion - function that returns the natural squire root of a number
  * @n: integer
  *
- * Return: Squire root of a number
+ * Return: Squire root or -1
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-	{
-		return (0);
-		return (check(1, n));
-	}
+if (n == 0)
+return (0);
+return (check(1, n));
 }
 /**
  * check - checks for the squire root
  * @a: interger
  * @b: interger
  *
- * Return: interger
+ * Return: int
  */
 
 int check(int a, int b)
 {
-	if (a * a > b)
-		return (-1);
-	else if (a * a == b)
-		return (a);
-	else
-		return (check(a + 1, b));
+if (a * a > b)
+return (-1);
+else if (a * a == b)
+return (a);
+else
+return (check(a + 1, b));
 }
